006697dfbfa2d537c1525510a2d7e5e13bfe9612
Jer Carlo Catallo <jcdcatallo@rpinnotech.com>
2020-11-11 16:13:19 +0800

    Account show,new and create action and routes completed

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,23 @@
 class AccountsController < ApplicationController
+
+    def show
+        @account = Account.find(params[:id])
+    end
+
+    def new
+        @account = Account.new
+    end
+
+    def create
+
+        @account = Account.new(params.require(:account).permit(:username,:firstname,:lastname,:email,:password))
+
+        if @account.save
+            redirect_to account_path(@account.id)
+        else
+            render 'new'
+        end
+
+    end
+
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,8 @@
 class Account < ApplicationRecord 
+
+    validates :username, presence:true, length:{minimum:5,maximum:25},uniqueness:{case_sensitive:false}
+    validates :firstname, presence:true
+    validates :lastname, presence:true
+    validates :email, presence:true, length:{minimum:5,maximum:25},uniqueness:{case_sensitive:false}
+    validates :password, presence:true
 end
